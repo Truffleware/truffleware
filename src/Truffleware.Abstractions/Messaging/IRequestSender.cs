75578d91b18b94dc4b4272613bcdab51b5df867e
Akke Luukkonen <6738134+akkeluukkonen@users.noreply.github.com>
2026-08-30 22:14:14 +0300

    Add Truffleware.Abstractions
    Currently only the messaging parts for trying to configure the code
    analysis & generators in this public repository.

--- src/Truffleware.Abstractions/Messaging/IRequestSender.cs
+++ src/Truffleware.Abstractions/Messaging/IRequestSender.cs
@@ -1,6 +1,7 @@
-namespace Truffleware.Abstractions;
+namespace Truffleware.Abstractions.Messaging;
 
 public interface IRequestSender<in TRequest, TResponse>
 {
     Task<TResponse> SendAsync(TRequest request);
 }
+
