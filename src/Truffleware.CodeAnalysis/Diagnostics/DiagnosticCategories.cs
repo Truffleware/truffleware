a4eacee4362b3bce1a1ee9fb18aa58a5ac18072c
Akke Luukkonen <6738134+akkeluukkonen@users.noreply.github.com>
2026-09-11 14:34:46 +0300

    Add DiagnosticCategories
    We could create our own but maybe it's easier to try to conform to these
    common ones? They look like a reasonable list anyway.

---
+++ src/Truffleware.CodeAnalysis/Diagnostics/DiagnosticCategories.cs
@@ -0,0 +1,22 @@
+namespace Truffleware.CodeAnalysis.Diagnostics;
+
+/// <summary>
+/// Copied from what Microsoft uses/recommends as categories.
+///
+/// https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/categories
+/// </summary>
+internal static class DiagnosticCategories
+{
+    internal const string Design = "Design";
+    internal const string Documentation = "Documentation";
+    internal const string Globalization = "Globalization";
+    internal const string Interoperability = "Interoperability";
+    internal const string Maintainability = "Maintainability";
+    internal const string Naming = "Naming";
+    internal const string Performance = "Performance";
+    internal const string SingleFile = "SingleFile";
+    internal const string Reliability = "Reliability";
+    internal const string Security = "Security";
+    internal const string Style = "Style";
+    internal const string Usage = "Usage";
+}
