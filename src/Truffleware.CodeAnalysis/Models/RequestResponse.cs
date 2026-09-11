99b1593425ca21cc3544ba267135819ff345121b
Akke Luukkonen <6738134+akkeluukkonen@users.noreply.github.com>
2026-09-11 14:45:09 +0300

    Add RequestResponseComparer
    While we want the generator pipeline to perform full equality checks,
    for our own purposes we don't really care about the `Location` that
    much. Hence, a custom comparer that ignores it.

--- src/Truffleware.CodeAnalysis/Models/RequestResponse.cs
+++ src/Truffleware.CodeAnalysis/Models/RequestResponse.cs
@@ -8,3 +8,40 @@ internal record RequestResponse(
     string ResponseName,
     ValueArray<string> ResponseNamespaces,
     LightweightLocation Location);
+
+internal class RequestResponseComparer : IEqualityComparer<RequestResponse>
+{
+    public bool Equals(RequestResponse? x, RequestResponse? y)
+    {
+        if (ReferenceEquals(x, y))
+        {
+            return true;
+        }
+
+        if (x is null || y is null)
+        {
+            return false;
+        }
+
+        if (x.GetType() != y.GetType())
+        {
+            return false;
+        }
+
+        // Skip location on purpose
+        return x.RequestName == y.RequestName &&
+               x.RequestNamespaces == y.RequestNamespaces &&
+               x.ResponseName == y.ResponseName &&
+               x.ResponseNamespaces == y.ResponseNamespaces;
+    }
+
+    public int GetHashCode(RequestResponse obj)
+    {
+        // Skip location on purpose
+        return HashCode.Combine(
+            obj.RequestName,
+            obj.RequestNamespaces,
+            obj.ResponseName,
+            obj.ResponseNamespaces);
+    }
+}
