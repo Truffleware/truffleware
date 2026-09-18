beb31dd1b8df69260b354086f7ed553ab1c89ccc
Akke Luukkonen <6738134+akkeluukkonen@users.noreply.github.com>
2026-09-18 14:08:37 +0300

    Add simple behavioral test
    Not much of a test, but it does confirm the sender got generated
    correctly.

--- tests/Truffleware.CodeAnalysis.Tests/Generators/RequestResponseGeneratorBehaviorTests.cs
+++ tests/Truffleware.CodeAnalysis.Tests/Generators/RequestResponseGeneratorBehaviorTests.cs
@@ -1,4 +1,9 @@
+using AutoFixture;
+using AutoFixture.AutoMoq;
+
 using Microsoft.Extensions.DependencyInjection;
+
+using Moq;
 
 using Truffleware.Abstractions.Messaging;
 using Truffleware.CodeAnalysis.Tests.Inputs;
@@ -6,9 +11,38 @@ using Truffleware.Generated;
 
 namespace Truffleware.CodeAnalysis.Tests.Generators;
 
-[TestClass]
 public class RequestResponseGeneratorBehaviorTests
 {
+    private readonly IFixture _fixture;
+
+    private readonly Mock<IRequestHandler<Ping, Pong>> _mockHandler;
+
+    protected RequestResponseGeneratorBehaviorTests()
+    {
+        _fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+        _mockHandler = _fixture.Freeze<Mock<IRequestHandler<Ping, Pong>>>();
+    }
+
+    [TestClass]
+    public class HandlerTests : RequestResponseGeneratorBehaviorTests
+    {
+        [TestMethod]
+        public async Task SendAsync_CorrectlyConfigured_Success()
+        {
+            var ping = _fixture.Create<Ping>();
+            var expected = new Pong();
+            _mockHandler.Setup(h => h.HandleAsync(ping)).ReturnsAsync(expected);
+
+            var sender = _fixture.Create<SenderPingToPong>();
+            var actual = await sender.SendAsync(ping);
+
+            _mockHandler.Verify(h => h.HandleAsync(ping), Times.Once);
+            _mockHandler.VerifyNoOtherCalls();
+            Assert.AreSame(expected, actual);
+        }
+    }
+
     [TestClass]
     public class ServiceExtensionTests : RequestResponseGeneratorBehaviorTests
     {
