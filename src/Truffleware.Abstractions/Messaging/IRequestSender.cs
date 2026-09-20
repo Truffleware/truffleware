7a088b5c8f4c7bc981dcbbc8dbe0f1d3692f4912
Akke Luukkonen <6738134+akkeluukkonen@users.noreply.github.com>
2026-09-20 09:43:11 +0300

    Add inline docs to abstractions

--- src/Truffleware.Abstractions/Messaging/IRequestSender.cs
+++ src/Truffleware.Abstractions/Messaging/IRequestSender.cs
@@ -1,5 +1,10 @@
 namespace Truffleware.Abstractions.Messaging;
 
+/// <summary>
+/// Sends a message to respective <see cref="IRequestHandler{TRequest,TResponse}"/>.
+/// </summary>
+/// <typeparam name="TRequest">Type sent to the handler.</typeparam>
+/// <typeparam name="TResponse">Type coming back from the handler.</typeparam>
 public interface IRequestSender<in TRequest, TResponse>
 {
     Task<TResponse> SendAsync(TRequest request);
--- src/Truffleware.Abstractions/Messaging/RequestHandlerAttribute.cs
+++ src/Truffleware.Abstractions/Messaging/RequestHandlerAttribute.cs
@@ -1,5 +1,13 @@
 namespace Truffleware.Abstractions.Messaging;
 
+/// <summary>
+/// Marker attribute for source generation.
+///
+/// Target class must be a partial class as source generation will make it inherit <see cref="IRequestHandler{TRequest,TResponse}"/>.
+/// Source generation will auto-generate a <see cref="IRequestSender{TRequest,TResponse}"/>.
+/// </summary>
+/// <typeparam name="TRequest">Incoming type sent to this handler.</typeparam>
+/// <typeparam name="TResponse">Outgoing response from this handler back to the sender.</typeparam>
 [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
 public sealed class RequestHandlerAttribute<TRequest, TResponse> : Attribute
 {
